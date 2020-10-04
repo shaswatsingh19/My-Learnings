559cf68c13429feec17c92695e0cc5b2b05fa9f6
Shaswat Singh <45266507+shaswatsingh19@users.noreply.github.com>
2020-10-04 21:01:35 +0530

    Add files via upload

---
+++ Algorithm/Number theory/Modular Exponentation.py
@@ -0,0 +1,22 @@
+# In python we use pow() function to use fast exponentiation
+# pow(x,e,m)
+# will provide the same result
+
+'''
+Algorithm:
+Here :-
+x = Base
+y = Exponent
+m = 1e9+7 or 1000000007
+'''
+def f(x,e,m):
+    y = 1
+    while e>0:
+        if x%2 == 0:
+            x = (x*x)%m
+            e = e//2
+        else:
+            y = (x*y)%m
+            e = e - 1
+    return y
+print(f(123323452,212345673344,1e9+7))
