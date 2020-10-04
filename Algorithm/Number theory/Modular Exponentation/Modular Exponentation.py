db06bcbd44c5939bef83a20ac00c019fe0e57062
Shaswat Singh <45266507+shaswatsingh19@users.noreply.github.com>
2020-10-04 23:41:01 +0530

    Update Modular Exponentation.py

--- Algorithm/Number theory/Modular Exponentation/Modular Exponentation.py
+++ Algorithm/Number theory/Modular Exponentation/Modular Exponentation.py
@@ -2,8 +2,31 @@
 # pow(x,e,m)
 # will provide the same result
 
+# Basic Method 
+# we just use pow(x,e) and that it but basic recursive and iterative algorithm
+
+def recursivePower(x,e):
+    
+    if e == 0:
+        return 1
+    return x*recursivePower(x,e-1)
+
+# We can also use iterative approach 
+
+def iterativePower(x,e):
+    ans = 1
+    while e>0:
+        ans = ans*x
+        e = e-1
+    return ans
+
 '''
-Algorithm:
+The above time complexity is O(n) but
+if we use large number like 10**19 it should be very  very slow
+thats why we use O(logn) approach
+'''
+'''
+Optimized method
 Here :-
 x = Base
 y = Exponent
