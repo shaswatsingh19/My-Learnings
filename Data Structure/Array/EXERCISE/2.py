11525254e9840b64cf13a68805d2f3c0190e4f28
Shaswat Singh <45266507+shaswatsingh19@users.noreply.github.com>
2020-10-07 16:22:05 +0530

    Add files via upload

---
+++ Data Structure/Array/EXERCISE/2.py
@@ -0,0 +1,8 @@
+# Write a Python program to multiplies all the items in a list.
+
+lst = [1,2,3,4,5,6]
+multiplies = 1
+for i in lst:
+    multiplies = multiplies * i
+
+print(multiplies)
---
+++ Data Structure/Array/EXERCISE/3.py
@@ -0,0 +1,7 @@
+#  Write a Python program to get the largest number from a list.
+
+lst = [2,5,1,5,16,7,2,55,21,65,12,67,99,654,11,22,10]
+
+maximum = max(lst)
+
+print(maximum)
