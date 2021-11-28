6b3816d5d4aa06d5203d160e8e2b0a5d44b6d2ce
shaswat singh <shaswatsingh.ose@gmail.com>
2021-11-28 20:18:29 +0530

    adding js exercise

---
+++ learn Javascript/Fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacci(n) {
+    
+	// write your solution here
+    if(n==0 || n==1){
+        return n;
+    }
+
+	return fibonacci(n-1)+fibonacci(n-2);
+}
+
+console.log(`fibonacci value at position 5: ${fibonacci(10)}`)
