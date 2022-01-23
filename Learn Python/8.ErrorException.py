0159e8964f520450cc13f1d34eb58f0b38639253
shaswat singh <shaswatsingh.ose@gmail.com>
2022-01-23 16:01:15 +0530

    exception

--- Learn Python/8.ErrorException.py
+++ Learn Python/8.ErrorException.py
@@ -1,3 +1,19 @@
 
 # while 1 print(1) syntax error due to :
-while 1:print(1)
+# while 1:print(1) # print(1) until we interrrupt
+
+# Exception => Error detected during execution
+'''
+TYPES =>
+ZeroDivisionError
+NameError
+TypeError and much more
+'''
+
+while True:
+    try:
+        a = input()
+        print(a)
+    except:
+        print("Value not provided")
+# infinite time ask for value
