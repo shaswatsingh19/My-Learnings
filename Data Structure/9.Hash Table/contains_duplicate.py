0dc0cd0c4d1d9833e0806bdb84e723ae8ccf72af
shaswat singh <shaswatsingh.ose@gmail.com>
2022-02-13 20:41:26 +0530

    contain duplicate

---
+++ Data Structure/9.Hash Table/contains_duplicate.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/contains-duplicate/
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        di = {}
+        
+        for i in nums:
+            if i not in di:
+                di[i] = 1
+            else:
+                return True
+            
+        return False
