70a39f672ccd38623c41cead537e41bce7dbf659
shaswat singh <shaswatsingh.ose@gmail.com>
2022-04-01 21:38:12 +0530

    find the last bad

---
+++ Daily Coding Problem/Amazon/first_bad_version.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/first-bad-version/
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        
+        low  = 1
+        high = n
+        ans = 1
+        while low<=high:
+            mid= (low+high)//2
+            
+            if isBadVersion(mid):
+                ans = mid
+                high = mid-1
+            else:
+                low = mid+1
+                
+                
+        return ans
