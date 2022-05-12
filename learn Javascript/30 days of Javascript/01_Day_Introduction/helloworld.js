3adee24b934bce254ee6cb18cb7aced9679b85b7
shaswat singh <shaswatsingh.ose@gmail.com>
2022-05-12 13:46:52 +0530

    complete day 1

---
+++ learn Javascript/30 days of Javascript/01_Day_Introduction/helloworld.js
@@ -0,0 +1,1 @@
+console.log('this is helloworld file from external  javascript and a example of multiple external js')
---
+++ learn Javascript/30 days of Javascript/01_Day_Introduction/index.js
@@ -0,0 +1,11 @@
+console.log('THis is external script')
+
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+let variableName = 'my variable'
+const PI = 3.14 
