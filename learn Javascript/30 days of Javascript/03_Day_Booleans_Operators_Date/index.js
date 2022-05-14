fb907f411a23040683e7022b228a1dd62f9b9cf1
shaswat singh <shaswatsingh.ose@gmail.com>
2022-05-14 20:42:14 +0530

    added day 3 solution

--- learn Javascript/30 days of Javascript/03_Day_Booleans_Operators_Date/index.js
+++ learn Javascript/30 days of Javascript/03_Day_Booleans_Operators_Date/index.js
@@ -105,80 +105,38 @@ let x = parseInt(prompt('enter side'))
 let y = parseInt(prompt('enter side'))
 let z = parseInt(prompt('enter side'))
 console.log(x+y+z)
-/*
 
-Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+// Compare your first name length and your family name length and you should get this output.
+firstName = prompt('firstname')
+lastName = prompt('last name')
 
-Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
-
-Calculate the slope, x-intercept and y-intercept of y = 2x -2
-
-Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
-
-Compare the slope of above two questions.
-
-Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
-
-Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
-
-Enter hours: 40
-Enter rate per hour: 28
-Your weekly earning is 1120
-If the length of your name is greater than 7 say, your name is long else say your name is short.
-
-Compare your first name length and your family name length and you should get this output.
-
-let firstName = 'Asabeneh'
-let lastName = 'Yetayeh'
-Your first name, Asabeneh is longer than your family name, Yetayeh
-Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
-
-let myAge = 250
-let yourAge = 25
-I am 225 years older than you.
-Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
-
-Enter birth year: 1995
-You are 25. You are old enough to drive
-
-Enter birth year: 2005
-You are 15. You will be allowed to drive after 3 years.
-Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
-
-Enter number of years you live: 100
-You lived 3153600000 seconds.
-Create a human readable time format using the Date time object
-
-YYYY-MM-DD HH:mm
-DD-MM-YYYY HH:mm
-DD/MM/YYYY HH:mm
-*/
+firstName.length > lastName.length ? console.log(`Your first name,${firstName} is longer than your family name, ${lastName}`) : console.log(`Your first name,${firstName} is smaller than your family name, ${lastName}`)
 
 
-// console.log('LEVEL 3')
+console.log('LEVEL 3')
 
-// // Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
-// // YYY-MM-DD HH:mm eg. 20120-01-02 07:05
-// now = new Date()
-// console.log(now)
-// year = now.getFullYear()
-// let month = now.getMonth()+1
-// if (month < 10){
-//     month = '0'+month
-// }
-// let currDate = now.getDate()
-// if (currDate < 10){
-//     currDate = '0'+currDate
-// }
+// Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
+// YYY-MM-DD HH:mm eg. 20120-01-02 07:05
+now = new Date()
+console.log(now)
+year = now.getFullYear()
+let month = now.getMonth()+1
+if (month < 10){
+    month = '0'+month
+}
+let currDate = now.getDate()
+if (currDate < 10){
+    currDate = '0'+currDate
+}
 
-// let hour = now.getHours()
-// if (hour < 10){
-//     hour = '0'+hour
-// }
+let hour = now.getHours()
+if (hour < 10){
+    hour = '0'+hour
+}
 
-// let minute  = now.getMinutes()
-// if (minute < 10){
-//     minute = '0'+minute
-// }
-// console.log(`${year}-${month}-${currDate} ${hour}:${minute}`)
+let minute  = now.getMinutes()
+if (minute < 10){
+    minute = '0'+minute
+}
+console.log(`${year}-${month}-${currDate} ${hour}:${minute}`)
 
