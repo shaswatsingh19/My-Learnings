e5472cfa7deaab42cfd1e58dd30373089c0540de
shaswat singh <shaswatsingh.ose@gmail.com>
2022-06-07 11:26:03 +0530

    completed day 16

---
+++ learn Javascript/30 days of Javascript/16_JSON/index.js
@@ -0,0 +1,144 @@
+console.log('LEVEL 1')
+
+const skills = ['HTML', 'CSS', 'JS', 'React','Node', 'Python']
+let age = 250;
+let isMarried = true
+const student = {
+  firstName:'Asabeneh',
+  lastName:'Yetayehe',
+  age:250,
+  isMarried:true,
+  skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+
+// Change skills array to JSON using JSON.stringify()
+
+const skillObj = JSON.stringify(skills)
+console.log(skillObj)
+
+// Stringify the age variable
+// console.log(age)
+const ageObj = JSON.stringify(age)
+
+// Stringify the isMarried variable
+const isMarriedObj = JSON.stringify(isMarried)
+
+// Stringify the student object
+const studentObj = JSON.stringify(student)
+console.log(studentObj)
+
+// Stringify the students object with only firstName, lastName and skills properties
+const StuObj = JSON.stringify(student,["firstName","lastName","skills"],2)
+console.log(StuObj)
+
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`
+
+// Parse the txt JSON to object.
+
+const users = JSON.parse(txt)
+console.log(users)
+
+// Find the user who has many skills from the variable stored in txt
+
+let maxSkill = 0
+for (let user of Object.keys(users)){
+    if(users[user].skills.length > maxSkill){
+        maxSkill = users[user].skills.length
+    }
+}
+console.log(maxSkill)
