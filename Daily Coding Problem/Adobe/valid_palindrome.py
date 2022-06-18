4cbecf82307c7bb0465e5e43a3e0b22bc8594745
shaswat singh <shaswatsingh.ose@gmail.com>
2022-06-18 20:32:14 +0530

    easy string

---
+++ Daily Coding Problem/Adobe/valid_palindrome.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/valid-palindrome/
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        i = 0
+        j = len(s)-1
+        
+        while i<j:
+            while i<j and not s[i].isalnum():
+                i+=1
+            
+            while i<j and not s[j].isalnum() :
+                j-=1
+            
+            if i<j and s[i].lower() != s[j].lower():
+                return False
+
+            i+=1
+            j-=1
+
+        return True
