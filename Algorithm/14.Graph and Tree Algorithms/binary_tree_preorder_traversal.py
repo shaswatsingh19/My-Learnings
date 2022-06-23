f119038f282af361095089c86edaf47a89a5ec27
shaswat singh <shaswatsingh.ose@gmail.com>
2022-06-23 13:16:22 +0530

    add iterative method to solve preorder

--- Algorithm/14.Graph and Tree Algorithms/binary_tree_preorder_traversal.py
+++ Algorithm/14.Graph and Tree Algorithms/binary_tree_preorder_traversal.py
@@ -7,19 +7,39 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack , res = [root , ] , [] # in O(N) time and O(N) space
+        while stack:
+            root = stack.pop()
+            if root:
+                res.append(root.val)
+                if root.right:
+                    stack.append(root.right)
+                if root.left:
+                    stack.append(root.left)
+        return res
+                    
+#         l = []
         
-        return self.pre(root,[])
+#         def pre(root):
+#             if root:
+#                 l.append(root.val)
+#                 pre(root.left)
+#                 pre(root.right)
+                
+                
+#         pre(root)
+#         return l
+        
+
+#         return self.pre(root,[])
         
         
-    def pre(self,root,l):
+#     def pre(self,root,l):
         
-        if root is None:
-            return 
-        
-        if root:
-            l.append(root.val)
-            self.pre(root.left,l)
-            self.pre(root.right,l)
+#         if root:
+#             l.append(root.val)
+#             self.pre(root.left,l)
+#             self.pre(root.right,l)
             
             
-        return l
+#         return l
--- Data Structure/6.Tree(Binary Tree)/preorder_traversal.py
+++ Data Structure/6.Tree(Binary Tree)/preorder_traversal.py
@@ -1,31 +1,45 @@
-"""
-This is the Node class definition
-
-class Node:
-	def __init__(self, data=0, left=None, right=None):
-		self.data = data
-		self.left = left
-		self.right = right
-
-"""
-# https://workat.tech/problem-solving/practice/binary-tree-preorder-traversal/
-
+# https://leetcode.com/problems/binary-tree-preorder-traversal/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-	def getPreorderTraversal(self, root: Optional[Node]) -> List[int]:
-		# add your logic here
-		l = []
-		self.preorder(root,l)
-		
-		return l
-	
-	def preorder(self,root,l):
-		
-		if (root):
-			l.append(root.data)
-			self.preorder(root.left,l)
-			self.preorder(root.right,l)
-			
-		
-		
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack , res = [root , ] , [] # in O(N) time and O(N) space
+        while stack:
+            root = stack.pop()
+            if root:
+                res.append(root.val)
+                if root.right:
+                    stack.append(root.right)
+                if root.left:
+                    stack.append(root.left)
+        return res
+                    
+#         l = []
+        
+#         def pre(root):
+#             if root:
+#                 l.append(root.val)
+#                 pre(root.left)
+#                 pre(root.right)
+                
+                
+#         pre(root)
+#         return l
+        
 
-
+#         return self.pre(root,[])
+        
+        
+#     def pre(self,root,l):
+        
+#         if root:
+#             l.append(root.val)
+#             self.pre(root.left,l)
+#             self.pre(root.right,l)
+            
+            
+#         return l
