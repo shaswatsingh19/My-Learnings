b50492b50ebb9cdbf5abfd2fcb8df6d0d1bbab67
shaswat singh <shaswatsingh.ose@gmail.com>
2022-07-20 12:30:12 +0530

    starting with react

---
+++ Learn React/React - Codedamn/hello-world/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+root.render(
+  <h1>Hello world by shaswat</h1>
+);
+
+
