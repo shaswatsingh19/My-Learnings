d298a4dcc888d979f5fd7f373c80f89edf75728e
shaswat singh <shaswatsingh.ose@gmail.com>
2022-10-05 20:09:56 +0530

    added files

--- Learn React/React - The complete Guide/02.Expense Calculator/expense-calculator/src/index.js
+++ Learn React/React - The complete Guide/02.Expense Calculator/expense-calculator/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react' not required to import explicitly for react 18
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './style.css'
